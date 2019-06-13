a1f0f82ed660fd1ede20af5281c7f6be36e4d6f3
Eric Clark <eclark0013@gmail.com>
2019-06-13 16:39:38 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,11 +3,12 @@ def prime?(x)
   primeness=true
   if x<2
     primeness=false
-  end
+  else
   composite_num_check=
     Array(2..Math.sqrt(x).floor).any? do |divisor|
       x%divisor == 0
     end
+  end
   if composite_num_check
     primeness=false
   end
